6ff465dd15d3fba92f58acfe3a351789f1ad7256
Tim Reynolds <tim@timothyreynolds.co.uk>
2019-01-01 16:29:45 +0000

    Update and format

--- example/index.js
+++ example/index.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Visible from 'Visible';
+import React from "react";
+import ReactDom from "react-dom";
+import Visible from "Visible";
 
 ReactDom.render(
   <div>
-    <Visible isVisible={ true }>
-      Text that is visible
-    </Visible>
-    <Visible isVisible= { false }>
-      Text that is NOT visible
-    </Visible>
-  </div>
-  ,
-  document.getElementById('root')
-  );
+    <Visible isVisible={true}>Text that is visible</Visible>
+    <Visible isVisible={false}>Text that is NOT visible</Visible>
+  </div>,
+  document.getElementById("root")
+);
--- src/Visible.js
+++ src/Visible.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default function Visible(props) {
   if (!props.isVisible) {
     return null;
   }
 
-  return (
-    <div>
-      { props.children }
-    < /div>
-    );
+  return <div>{props.children}</div>;
 }
 
 Visible.propTypes = {
   isVisible: React.PropTypes.bool.isRequired,
-  children: React.PropTypes.any.isRequired,
+  children: React.PropTypes.any.isRequired
 };
